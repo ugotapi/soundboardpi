b1dac6e1d4b591832c83233116d385a61c57563a
Ugotapi <rudischmitz@gmail.com>
2024-01-24 20:47:13 -0600

    Update play.php

--- play.php
+++ play.php
@@ -1,4 +1,4 @@
-//  for using the HDMI sound you can put "-ao pulse"
+//  for using the HDMI sound you can remove "-ao alsa:device=hw=2,0". Assumes a USB audio device to speaker setup
 <?php
 $sound = $_GET['sound']; // Retrieves the value of the "sound" parameter from the URL
 exec('/usr/bin/mplayer -vo null -ao alsa:device=hw=2,0 /var/www/html/$sound');
